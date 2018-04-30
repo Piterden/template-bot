c3844a751c246bca84574c06a34a2764d1883500
Denis Efremov <efremov.a.denis@gmail.com>
2018-04-30 10:44:28 +0300

    Added env for debug.

--- template/src/helpers/debug/index.js
+++ template/src/helpers/debug/index.js
@@ -1,8 +1,10 @@
 const { inspect } = require('util')
 
 
+const { DEBUG_COLORS, DEBUG_SHOW_HIDDEN, DEBUG_DEPTH } = process.env
+
 module.exports = (data) => console.log(inspect(data, {
-  colors: true,
-  showHidden: true,
-  depth: 10,
+  colors: DEBUG_COLORS,
+  showHidden: DEBUG_SHOW_HIDDEN,
+  depth: DEBUG_DEPTH,
 }))
